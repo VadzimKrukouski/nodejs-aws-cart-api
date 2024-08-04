6340527fcc1a7e4d8df9c2bb0a8d632975ff84fd
Vadim Krukovski <vadim.krukovski@pstlabs.by>
2024-08-04 14:57:44 +0300

    task-9. Docker and AWS Elastic Beanstalk

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,21 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { Handler, Context } from 'aws-lambda'
-import { ExpressAdapter } from '@nestjs/platform-express'
-import { Server } from 'http'
-import * as express from 'express'
-import * as awsServerlessExpress from 'aws-serverless-express'
+import { NestFactory } from '@nestjs/core';
 
-let server: Server
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const expressApp = express()
-  const expressAdapter = new ExpressAdapter(expressApp)
-  const appFactory = await NestFactory.create(AppModule, expressAdapter)
-  appFactory.enableCors({
-    origin: '*',
-    methods:'GET,HEAD,PUT,PATCH, POST, DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization'
-  })
-  await appFactory.init()
-  return awsServerlessExpress.createServer(expressApp)
+  const app = await NestFactory.create(AppModule);
 
-}
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
 
-export const handler: Handler = async (event: any, context: Context) => {
-  server = server || (await bootstrap())
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise
+  await app.listen(port);
 }
+bootstrap().then(() => {
+  console.log('App is running on %s port', port);
+});
